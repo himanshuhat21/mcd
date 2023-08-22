8bd78b0eb33fe3335ef69874a64586411b9292df
Himanshu Biswas <99411982+himanshuhat21@users.noreply.github.com>
2023-08-22 05:39:07 -0700

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char out;
+    cout << "Enter your choice of ICE CREAM: ";
+    cin >> out;
+    if(tolower(out)=='choclate'){
+        cout>>"ICE CREAM of your choice is " << out << endl;
+    }
+    else if (tolower(out)=='vanilla'){
+        cout>>"ICE CREAM OF YOUR CHOICE IS: " << out << endl;
+    }
+    else{
+        cout>>"Invalid." << out << endl;
+    }
+    return 0;
+}
